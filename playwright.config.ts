196915da342f4c29023d1ff3f5bc63a65e58ddb7
doanthuc12 <nguyentrandoanthuc1212@gmail.com>
2025-08-17 09:57:46 +0700

    test: trigger CI on PR

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const isCI = !!process.env.CI;
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -10,11 +12,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: isCI,
     screenshot: "only-on-failure",
     video: "retain-on-failure",
     trace: 'on-first-retry',
